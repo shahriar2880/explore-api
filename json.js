062ed438852ac2535f09bfb0e67feaa4e27db886
shahriar2880 <shahriarreza436@gmail.com>
2024-10-17 03:41:44 +0600

    second commit

--- json.js
+++ json.js
@@ -1,5 +1,20 @@
 const user = {Name: 'shahriar', Id: 192352880};
 //javascript obejct notation
 const stringified = JSON.stringify(user);
-console.log(user);
-console.log(stringified);
+// console.log(user);
+// console.log(stringified);
+
+const shop ={
+    owner: 'Shahriar',
+    address: {
+        street: 'satmosjid road',
+        city: 'dhaka',
+        country: 'bangladesh'
+    },
+    products:['laptop', 'headphone','monitor'],
+    revenue: 10000000,
+    isOpen:true,
+    isNew: false
+}
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
